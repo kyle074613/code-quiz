62c6bff257d5812225d46170f28e3ff2023ca2dd
Kyle Jones <kyle074613@gmail.com>
2019-12-14 12:23:22 -0800

    worked on populating the quiz page, needs more work

--- script.js
+++ script.js
@@ -37,10 +37,48 @@ startQuizButton.addEventListener("click", function () {
     }, 1000);
 
 
-    // var questionEl = document.createElement("div");
-    // var optionsEl = document.createElement("div");
+    var questionEl = document.createElement("div");
+    var optionsEl = document.createElement("div");
+    questionEl.className = "row justify-content-center mt-5";
+    optionsEl.className = "row mt-4";
+    containerEl.appendChild(questionEl);
+    containerEl.appendChild(optionsEl);
+
+    var questionTextEl = document.createElement("h2");
+    // var optionsListEl = document.createElement("ul");
+    var optionsListEl = document.createElement("div");
+    optionsListEl.className = "col-8 mx-auto";
+    questionTextEl.textContent = quiz.questions[questionCount];
+    questionEl.appendChild(questionTextEl);
+    optionsEl.appendChild(optionsListEl);
+
+    // var optionOneEl = document.createElement("li");
+    // var optionTwoEl = document.createElement("li");
+    // var optionThreeEl = document.createElement("li");
+    // var optionFourEl = document.createElement("li");
+
+    var optionOneEl = document.createElement("button");
+    var optionTwoEl = document.createElement("button");
+    var optionThreeEl = document.createElement("button");
+    var optionFourEl = document.createElement("button");
+    optionOneEl.className = "btn btn-primary";
+    optionTwoEl.className = "btn btn-primary";
+    optionThreeEl.className = "btn btn-primary";
+    optionFourEl.className = "btn btn-primary";
+    //
+    optionOneEl.textContent = quiz.options.split(",")[0];
+    optionTwoEl.textContent = quiz.options.split(",")[1];
+    optionThreeEl.textContent = quiz.options.split(",")[2];
+    optionFourEl.textContent = quiz.options.split(",")[3];
+    //
+    optionsListEl.appendChild(optionOneEl);
+    optionsListEl.appendChild(optionTwoEl);
+    optionsListEl.appendChild(optionThreeEl);
+    optionsListEl.appendChild(optionFourEl);
+
+    //continue working on appending
+
 })
 
-var questionEl = document.createElement("div");
 
 
